17a2d226e561289bb481b2f067bf2c78adf6cd95
Alejandra Muñoz Villalobos <alemv18@hotmail.com>
2015-11-02 12:54:02 -0600

    Update jerarquia.py

--- _models/jerarquia.py
+++ _models/jerarquia.py
@@ -3,13 +3,28 @@ from __init__ import *
 class Jerarquia(Model):
     nombre = CharField(max_length=100)
     superior = CharField(max_length=100)
+    # empleado = CharField(max_length=100)
+    # colega = CharField(max_length=100)
 
     class Meta:
         database = DATABASE
-        
+
+ @classmethod
+    def nueva(cls, nombre, superior):
+        cls.create(
+                nombre=nombre
+                superior=superior
+
+        )
     def to_json(self):
-        return{
-    "nombre" = self.nombre,
-    "superior"= self.superior
-    }
-        
+        return {
+            "nombre" : self.nombre
+            "superior" : self.superior
+        }
+
+    # def get_jerarquia(self):
+    #     jerarquia = Respuesta.select().where(Respuesta.pregunta == self.id)
+    #     return respuestas
+
+    # def __repr__(self):
+    #     return self.pregunta9        
