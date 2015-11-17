5451cad0680f5eb5f28b5d52515e2bb0be1251db
alemv18 <alemv18@hotmail.com>
2015-11-17 09:31:10 -0600

    unos cambios del dia de hoy

---
+++ tests/testRespuestas.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+url = "http://localhost:5000/"
+
+def crearCompetencias():
+	pass
+
+def testCrearCompetencia():
+	pass
+
+def testActualizarUsuario():
+	pass
+
+def testConsultarUsuario():
+pass
+
+def testBorrarUsuario():
+	pass
+
+def testConsultarUsuarios():
+	pass
+
+
+
+
+
+if __name__ == '__main__':
+	crearCompetencias()
+
+	testCrearCompetencia()
+	testActualizarCompetencia()
+	testConsultarCompetencia()
+	testBorrarCompetencia()
+	testConsultarCompetencia()
